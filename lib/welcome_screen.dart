e94ff4c2b4450811e24da0a3474f1468f8ef2e0a
Rehman-Khan1 <abdulrehmanabdulahad81@gmail.com>
2025-05-09 02:12:55 +0500

    Login signup page and start screen

--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
-import 'home_screen.dart';
+import 'start_screen.dart'; // 👉 Import your new screen
 
 class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
@@ -13,11 +13,11 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   void initState() {
     super.initState();
-    // Navigate after 3 seconds
+    // Navigate to StartScreen after 3 seconds
     Timer(const Duration(seconds: 3), () {
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => const HomeScreen()),
+        MaterialPageRoute(builder: (context) => StartScreen()), // 👈 go to new screen
       );
     });
   }
